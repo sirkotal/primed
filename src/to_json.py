89242b6972adb47ca03abe3b2dbccfcdc67e98de
Emanuel Maia <tavanomanu@gmail.com>
2024-09-30 17:37:07 +0100

    update script

--- src/to_json.py
+++ src/to_json.py
@@ -3,12 +3,12 @@ import json
 
 data = {}
 
-with open("./Medicine_Details.csv", encoding="utf-8") as csvf:
+with open("../dataset/Medicine_Details.csv", encoding="utf-8") as csvf:
     csvReader = csv.DictReader(csvf)
 
     for row in csvReader:
         key = row["Medicine Name"]
         data[key] = row
 
-with open("./Medicine_Details.json", "w", encoding="utf-8") as jsonf:
+with open("../dataset/Medicine_Details.json", "w", encoding="utf-8") as jsonf:
     jsonf.write(json.dumps(data, indent=4))
