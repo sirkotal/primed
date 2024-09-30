5e1267c3393375af1b259b2a094093a311726fe9
sirkotal <lordkotal2002@gmail.com>
2024-09-30 17:31:03 +0100

    Refactored into function

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,13 @@
 import csv
 
-data_list = []
-
-with open('./dataset/Medicine_Details.csv', mode='r') as file:
+def convert_data_to_maps(lst, file):
     csv_reader = csv.DictReader(file)
     
     for med in csv_reader:
-        data_list.append(dict(med))
+        lst.append(dict(med))
+
+data_list = []
+file = open('./dataset/Medicine_Details.csv', mode='r')
+convert_data_to_maps(data_list, file)
 
 print(data_list)
