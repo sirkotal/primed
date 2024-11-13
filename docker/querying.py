21a9273d62feb7cab683f0897e3f8f5559627afc
sirkotal <lordkotal2002@gmail.com>
2024-11-13 19:52:26 +0000

    Working, but term boosters need to be reviewed

--- docker/querying.py
+++ docker/querying.py
@@ -4,16 +4,20 @@ def generate_simple_query(user_query):
         "start": 0,
         "rows": 30,
         'defType': 'edismax',
+        'qf': "drug composition applicable_diseases diseases_info possible_side_effects reviews manufacturer manufacturer_desc"
     }
     return query_params
 
 def generate_boosted_query(user_query):
     boosted_terms = {"cure": 3.0, "progress": 3.0, "effective": 2.5, "hope": 1.5, "side effects": 3.0, "risk": 2.5, "concern": 1.5}
+    boosted_query = []
     for term in user_query.split():
         if term in boosted_terms:
-            boosted_query = " ".join(f"{term}^{boosted_terms[term]}")
+            boosted_query.append(f"{term}^{boosted_terms[term]}")
         else:
-            boosted_query = term
+            boosted_query.append(term)
+    
+    boosted_query = " ".join(boosted_query)
     query_params = {
         'q': boosted_query,
         "sort": "reviews_average_rating desc",
