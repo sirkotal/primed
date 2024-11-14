d445016a4f0faf611af1454479142b2caa54b4ef
Emanuel Maia <tavanomanu@gmail.com>
2024-11-14 17:26:51 +0000

    Improve text cleanup functions

--- data_pipeline/text_cleanup.py
+++ data_pipeline/text_cleanup.py
@@ -2,12 +2,29 @@ import wordninja
 import re
 
 
+def split_midword_uppercase(word):
+    i = 0
+    while i < len(word):
+        # For cases where the previous character is also uppercase I assume it's a sequence of initials
+        # (don't break it)
+        if word[i].isupper() and i - 1 >= 0 and word[i - 1] != ' ' and word[i - 1].islower(): 
+            word = word[0:i] + ' ' + word[i:len(word)] 
+        i += 1
+    return word
+
+
 def concat_with_punctuation(words):
     string = ""
-    punctuation = [',', '.', ';', ':', '\'', '`', '!', '?']
+    no_space_on_either_side = ['-', '/']
+    no_space_on_right_side = ['[', '(', '{'] + no_space_on_either_side
+    no_space_on_left_side = [']', ')', '}', ',', '.', ';', ':', '\'', '`', '!', '?'] + no_space_on_either_side
     for idx, word in enumerate(words):
         string += word
-        if idx > 0 and idx + 1 < len(words) and words[idx + 1] not in punctuation:
+        if idx > 0 and idx + 1 < len(words) \
+            and words[idx + 1] not in no_space_on_left_side \
+            and words[idx] not in no_space_on_right_side:
+            string += ' '
+        elif idx == 0 and words[idx] not in no_space_on_right_side:
             string += ' '
     return string
 
@@ -25,4 +42,4 @@ def repair_string(string):
     string = remove_newlines_and_extra_spaces(string)
     words = wordninja.split(string)
     words = list(filter(lambda ch: ch != ' ', words))
-    return concat_with_punctuation(words)
+    return split_midword_uppercase(concat_with_punctuation(words))
