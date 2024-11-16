3ec429af3441d8fc17da16b7d7181270939dfb37
sirkotal <lordkotal2002@gmail.com>
2024-11-16 19:10:53 +0000

    Updated report and added some terms to be boosted

--- docker/querying.py
+++ docker/querying.py
@@ -11,7 +11,8 @@ def generate_simple_query(user_query):
     return query_params
 
 def generate_boosted_query(user_query):
-    boosted_terms = {"cure": 4.0, "progress": 3.0, "prevent": 2.75, "effective": 2.5, "safe": 2.5, "hope": 1.5, "side effects": 3.0, "risk": 2.5, "concern": 1.5}
+    boosted_terms = {"cure": 4.0, "progress": 3.0, "prevent": 2.75, "effective": 2.5, "safe": 2.5, "hope": 1.5, "side effects": 3.0, 
+                     "risk": 2.5, "concern": 1.5, "reliable": 1.5, "aggressive": 1.5, "death": 2, "prestigious": 1.1}
     boosted_query = []
     for term in user_query.split():
         if len(term) > 5:
