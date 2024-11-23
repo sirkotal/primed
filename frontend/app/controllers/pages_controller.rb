867dc1797619b729e2366742bbe92b109dc4f7dc
sirkotal <lordkotal2002@gmail.com>
2024-11-23 10:45:00 +0000

    Working on the frontend of the search engine

--- frontend/app/controllers/pages_controller.rb
+++ frontend/app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def index
   end
+  def search
+    query = params[:query]
+    search_type = params[:type] || 'baseline'
+    api_url = 'http://127.0.0.1:8000'
+
+    response = HTTP.post(api_url, form: { query: query })
+
+    if response.status.success?
+      @results = response.parse['results']
+    else
+      @error = response.parse['error'] || "An error occurred"
+    end
+
+    render :search_results
+  end
 end
