e290b38881f94798fbf177045ef5e02ee39979a9
Simoes <pedrosimoes.ps@hotmail.com>
2024-12-11 12:14:31 +0000

    Update to make semantic query

--- docker/querying.py
+++ docker/querying.py
@@ -1,3 +1,13 @@
+from sentence_transformers import SentenceTransformer
+
+def text_to_embedding(text):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    embedding = model.encode(text, convert_to_tensor=False).tolist()
+    
+    # Convert the embedding to the expected format
+    embedding_str = "[" + ",".join(map(str, embedding)) + "]"
+    return embedding_str
+
 def generate_simple_query(user_query):
     query_params = {
         'q': user_query,
@@ -31,6 +41,42 @@ def generate_boosted_query(user_query):
 
     query_params = {
         'q': user_query,
+        'q.op': "AND",
+        'sort': "score desc",
+        'start': 0,
+        'rows': 100,
+        'fl': "id, drug, composition, applicable_diseases, diseases_info, possible_side_effects, excellent_review_perc, average_review_perc, poor_review_perc, reviews_average_rating, reviews_useful_count, reviews, manufacturer, manufacturer_desc, manufacturer_start, manufacturer_end, score",
+        'defType': "edismax",
+        'qf': "diseases_info^3 reviews^4 manufacturer_desc",
+        'pf': "reviews^3",
+        'ps': 2,
+        # 'bf': f"excellent_review_perc^1.5 poor_review_perc^0.5 query({{!v='side_effects:{user_query}'}})^1.5"
+        'bf': f"excellent_review_perc^1.5 poor_review_perc^0.5"
+    }
+    return query_params
+
+def generate_semantic_boosted_query(user_query):
+    '''boosted_terms = {"cure": 4.0, "progress": 3.0, "prevent": 2.75, "effective": 2.5, "safe": 2.5, "hope": 1.5, "effects": 3.0, 
+                     "risk": 2.5, "concern": 1.5, "reliable": 1.5, "aggressive": 1.5, "death": 2.0, "prestigious": 1.1}
+
+        if term in boosted_terms:
+            boosted_query.append(f"{term}^{boosted_terms[term]}")
+        else:
+            boosted_query.append(term)
+    
+    boosted_query = " ".join(boosted_query)'''
+
+    embedding = text_to_embedding(user_query)
+
+    boosted_query = []
+
+    for term in user_query.split():
+        if len(term) > 5:
+            term = f"{term}~1"
+        boosted_query.append(term)
+
+    query_params = {
+        'q': f"{user_query}{embedding}",
         'q.op': "AND",
         'sort': "score desc",
         'start': 0,
---
+++ docker/request_semantic.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from querying import generate_semantic_boosted_query
+
+solr = 'http://localhost:8983/solr/primed-data/select'
+
+query = input("Enter your query: ")
+
+query_params = generate_semantic_boosted_query(query)
+
+response = requests.get(solr, params=query_params)
+
+if response.status_code == 200:
+    print(f"Successful query.")
+    print(f"SOLR Response: {response.json()}")
+
+    with open("solr_adv_response.json", "w") as file:
+        json.dump(response.json(), file, indent=4) 
+else:
+    print(f"Error: {response.status_code}")
